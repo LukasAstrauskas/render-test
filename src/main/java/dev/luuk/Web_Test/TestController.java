62f91a4ee08ce1913d3445c55e65171c57ebd383
Lukas <lukas.astrauskas@gmail.com>
2023-10-16 19:40:26 +0300

    add pathvar

--- src/main/java/dev/luuk/Web_Test/TestController.java
+++ src/main/java/dev/luuk/Web_Test/TestController.java
@@ -1,6 +1,7 @@
 package dev.luuk.Web_Test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -13,6 +14,11 @@ public class TestController {
     @GetMapping
     public String test() {
         return "Hello, Render.";
+    }
+
+    @GetMapping("{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello, " + name + ".";
     }
 
 }
